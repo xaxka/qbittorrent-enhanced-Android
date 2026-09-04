55b29df41de801c1f6a02f9466e83c8b71e73329
xaxka <xaxka@users.noreply.github.com>
2026-09-04 07:01:17 +0000

    fix(kotlin): FragmentStateAdapter import path (androidx.viewpager2.adapter, not androidx.fragment.app)

--- app/src/main/java/io/github/xixka/qbittorrent/ui/qbsettings/QBSettingsFragment.kt
+++ app/src/main/java/io/github/xixka/qbittorrent/ui/qbsettings/QBSettingsFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentStateAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
--- app/src/main/java/io/github/xixka/qbittorrent/ui/rss/RssFragment.kt
+++ app/src/main/java/io/github/xixka/qbittorrent/ui/rss/RssFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentStateAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
